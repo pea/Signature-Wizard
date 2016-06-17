800267041fd276338373d09e6511a6561768c17a
Peter Bailey <prodigy.zero@gmail.com>
2016-06-17 12:56:53 +0000

    Automation

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+var gulp = require('gulp'),
+    runSequence = require('run-sequence'),
+    watch = require('gulp-watch'),
+    zip = require('gulp-zip'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    htmlmin = require('gulp-htmlmin');
+
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.*', ['serve']);
+});
+
+gulp.task('sass', function() {
+    return gulp.src('src/scss/*.scss').
+    pipe(sass.sync().on('error', sass.logError)).
+    pipe(sass({
+        outputStyle: 'compressed'
+    }).on('error', sass.logError)).
+    pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('compress', function() {
+    return gulp.src('src/js/*.js').
+    pipe(uglify()).
+    pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('htmlmin', function() {
+    return gulp.src('src/*.html').
+    pipe(htmlmin({
+        collapseWhitespace: true
+    })).
+    pipe(gulp.dest('dist'));
+});
+
+gulp.task('copyimages', function() {
+    return gulp.src('src/images/**/*.{png,gif,jpeg,jpg}').
+    pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('copymanifest', function() {
+    return gulp.src('src/manifest.json').
+    pipe(gulp.dest('dist'));
+});
+
+gulp.task('copylocales', function() {
+    return gulp.src('src/_locales/**/*.*').
+    pipe(gulp.dest('dist/_locales'));
+});
+
+gulp.task('zip', function() {
+    return gulp.src('src/*').
+    pipe(zip('dist.zip')).
+    pipe(gulp.dest('dist'));
+});
+
+gulp.task('dist', function(callback) {
+    runSequence(
+        'sass',
+        'compress',
+        'htmlmin',
+        'copyimages',
+        'copymanifest',
+        'copylocales',
+        'zip',
+        callback);
+});
+
+gulp.task('serve', function(callback) {
+    runSequence(
+        'sass',
+        'compress',
+        'htmlmin',
+        'copyimages',
+        'copymanifest',
+        'copylocales',
+        callback);
+});
---
+++ src/js/options.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+    app.init();
+});
+var app = {    
+    init: function(){
+        app.loadValues();
+        app.submit();
+        $('.add-set').on('click', function(){
+            app.addSet(); 
+        });
+        $('.sets').on('click', function(el){
+            $('.sets input').removeClass('highlighted');
+            $($(el.target).parents('.set')[0]).find('input').addClass('highlighted');
+        });
+        $('a').on('click', function(e){
+            e.preventDefault();
+            chrome.tabs.create({ url: $(this).attr('href') });
+        });
+    },    
+    loadValues: function(){
+        chrome.storage.sync.get(null, function (obj) {
+            
+            if(!obj.data){
+                $('.sets').append(app.templates.set('', ''));
+            }
+            
+            $(obj.data).each(function(i,v) {
+                if(v.email !== '' && v.url !== ''){
+                    $('.sets').append(app.templates.set(v.email, v.url));
+                }
+            });
+            
+        });  
+    },   
+    addSet: function(){
+        $('.sets').append(app.templates.set('', ''));
+    },   
+    chrome: {    
+        /**
+        * Save
+        * Writes data with Chrome's storage API
+        * obj Object of {name: 'field name', value: 'field value'}
+        */
+        save: function(obj){
+            $(obj).each(function(i,v) {
+                chrome.storage.sync.set(obj);
+            });
+        }      
+    },        
+    submit: function(){        
+        /**
+        * Submit
+        * Saves form data with Chrome's storage API on submission
+        */       
+        $('form').on('submit', function(e){
+            e.preventDefault;           
+            var sets = [];
+            $('.set').each(function(i,v) {
+                if($(v).find('input[class="email"]').val() !== '' && $(v).find('input[class="url"]').val() !== ''){
+                    sets.push({
+                        'email': $(v).find('input[class="email"]').val(),
+                        'url': $(v).find('input[class="url"]').val()
+                    }); 
+                }
+            });            
+            app.chrome.save({'data': sets});           
+            $('.status').html('Saved').fadeIn(300).fadeOut(3000);
+        });
+    },
+    templates: {
+        /*
+        * Set
+        * Email and URL input fields
+        */
+        set: function(email, url){
+            return '<div class="set"><input type="email" value="' + email + '" placeholder="Email address" class="email"><input type="url" value="' + url + '" placeholder="Url of signature (https:// only)" class="url"></div>';
+        }
+    }
+};
